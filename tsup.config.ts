023c11686ddc8e3fd139629692e518ea0d9d572b
Cameron Matheson <cameron@workos.com>
2024-07-19 15:08:44 -0600

    updated config && states

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
-  splitting: true,
+  splitting: false,
   sourcemap: true,
   clean: true,
 });
