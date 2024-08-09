1940877bd23416cb38813eec8cd4c7607ec20074
Cameron Matheson <cameron@workos.com>
2024-08-09 15:46:55 -0600

    schedule proactive refreshes after code is exchanged

--- src/create-client.ts
+++ src/create-client.ts
@@ -186,6 +186,7 @@ export async function createClient(
 
           if (authenticationResponse) {
             _authkitClientState = "AUTHENTICATED";
+            _scheduleAutomaticRefresh();
             setSessionData(authenticationResponse, { devMode });
             _onRefresh && _onRefresh(authenticationResponse);
             onRedirectCallback({ state, ...authenticationResponse });
