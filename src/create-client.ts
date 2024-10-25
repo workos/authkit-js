36eb1fa8be2dfe3cccd96bd87327071b617b1a38
Michael Hadley <m@mthadley.com>
2024-10-25 11:54:38 -0700

    Add option to configure refresh buffer interval
    `refreshBufferInterval` representing how long before the current access
    token expires to start attempting to refresh.

--- src/create-client.ts
+++ src/create-client.ts
@@ -53,6 +53,7 @@ export async function createClient(
     onRedirectCallback = (_: RedirectParams) => {},
     onRefresh: _onRefresh,
     onRefreshFailure: _onRefreshFailure,
+    refreshBufferInterval = 10,
   } = options;
 
   const _clientId = clientId;
@@ -82,9 +83,8 @@ export async function createClient(
           return true;
         }
 
-        // TODO: should LEEWAY be configurable?
-        const LEEWAY = 10 * 1000; // 10 seconds
-        const refreshTime = expiresAt - LEEWAY;
+        const tokenRefresBufferInSeconds = refreshBufferInterval * 1000;
+        const refreshTime = expiresAt - tokenRefresBufferInSeconds;
         return refreshTime < Date.now();
     }
   };
